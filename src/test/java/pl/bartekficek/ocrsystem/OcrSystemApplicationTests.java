87c040bc753f723256cfbd391d9133fa2a9c4d66
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-12-21 15:15:13 +0100

    tess4J implemented in class TesseractExample, reading text works

---
+++ src/test/java/pl/bartekficek/ocrsystem/OcrSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bartekficek.ocrsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
