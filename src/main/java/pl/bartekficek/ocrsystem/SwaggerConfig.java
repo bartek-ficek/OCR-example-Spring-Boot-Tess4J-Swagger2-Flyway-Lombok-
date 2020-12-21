90ea47b813c2b961dc4b55ee449c8862ea77a36e
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-12-21 21:08:22 +0100

    SwaggerConfig created, Swagger works

---
+++ src/main/java/pl/bartekficek/ocrsystem/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package pl.bartekficek.ocrsystem;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    // URL to writing out in browser (Swagger GUI): http://localhost:8081/swagger-ui.html
+
+}
