14c7cd0d7bb17ce8e60373112f5eafb637c29fd0
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-12-21 21:40:13 +0100

    db configuration ceated in application.properties

--- src/main/java/pl/bartekficek/ocrsystem/OCRServiceApi.java
+++ src/main/java/pl/bartekficek/ocrsystem/OCRServiceApi.java
@@ -1,9 +1,12 @@
 package pl.bartekficek.ocrsystem;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class OCRServiceApi {
@@ -19,4 +22,10 @@ public class OCRServiceApi {
     public String doOCR(@RequestBody Graphic graphic) {
         return ocrService.doOCR(graphic.getUrl());
     }
+
+    @GetMapping("/api/ocr")
+    public List<Graphic> getGraphics() {
+        return //todo
+    }
+
 }
