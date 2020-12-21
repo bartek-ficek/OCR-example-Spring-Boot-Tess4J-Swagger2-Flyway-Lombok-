85a67a17605238bb35330f43834854e48ad5090d
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-12-21 22:22:09 +0100

    OCRServiceApi modified, start of work with Flyway library

--- src/main/java/pl/bartekficek/ocrsystem/OCRServiceApi.java
+++ src/main/java/pl/bartekficek/ocrsystem/OCRServiceApi.java
@@ -6,26 +6,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class OCRServiceApi {
 
     private OCRService ocrService;
+    private GraphicRepo graphicRepo;
 
     @Autowired
-    public OCRServiceApi(OCRService ocrService) {
+    public OCRServiceApi(OCRService ocrService, GraphicRepo graphicRepo) {
         this.ocrService = ocrService;
+        this.graphicRepo = graphicRepo;
     }
 
     @PostMapping("/api/ocr")
-    public String doOCR(@RequestBody Graphic graphic) {
-        return ocrService.doOCR(graphic.getUrl());
+    public Graphic doOCR(@RequestBody Graphic graphic) {
+        String ocrResult = ocrService.doOCR(graphic.getUrl());
+        graphic.setContent(ocrResult);
+        return graphicRepo.save(graphic);
     }
 
     @GetMapping("/api/ocr")
-    public List<Graphic> getGraphics() {
-        return //todo
+    public Iterable<Graphic> getGraphics() {
+        return graphicRepo.findAll();
     }
-
 }
